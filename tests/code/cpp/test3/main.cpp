a1723da26288e8a347e42e7d96faede5e49bf891
LeGusto <janusonis.gustas@gmail.com>
2025-06-29 22:16:09 +0300

    Added an input test for C++

---
+++ tests/code/cpp/test3/main.cpp
@@ -0,0 +1,14 @@
+// Solves the Coin Change problem using DP
+// outputs the minimum number of coins needed, -1 if impossible.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string a;
+    std::cin >> a;
+    std::cout << a << endl;
+    return 0;
+}
